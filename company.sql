47c851243b5f7dd06ebd23b19e2db206563a29c3
Dryusen <masalovandreyn@gmail.com>
2025-05-12 22:31:53 +0300

    Add files

---
+++ company.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE company;
+USE company;
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    age INT,
+    position VARCHAR(255),
+    salary FLOAT
+);
